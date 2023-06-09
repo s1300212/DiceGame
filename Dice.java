462426378cce3a8f93480b04cd6d5d84f3179de4
s1300212 <s1300212@u-aizu.ac.jp>
2023-06-09 12:35:36 +0900

    Dice game:initial revision

---
+++ Dice.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+public class Dice{
+    public static void main(String[] args){
+	int sum=0;
+	Random rand = new Random();
+	int m = rand.nextInt(6)+1;
+	int a = rand.nextInt(6)+1;
+	sum += (m+a);
+	System.out.println("Rolling dice...");
+	System.out.println("Die 1 :"+ m);
+	System.out.println("Die 2 :"+ a);
+	System.out.println("Total value:"+ sum);
+    }
+
+}
